35ad13a889e6e64374feb2e1be6748670b2e4afa
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-08 15:46:27 +0200

    Task resolve

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-const MyMath = require('./MyMath');
+const fs = require('fs').promises;
 
 /*
-HOW require() works:
-1. Resolve - розв'язування
-    1) Core modules
-    2) node_modules/
-    3) local (in your project folder)
-        3.1. Directory
-                index.js | index.json
-        3.2 Filename
-                name.js | name.json
-    4) throw new Error
-2. Loading - завантаження
-3. Wrappening - огортання
-4. Evaluation - виконання
-5. Caching - кешування (запам'ятовування)
+Задача: прочитати вміст текстового файлу і вивести його на консоль
 */
+
+const promise = fs.readFile('./text.txt', 'utf-8')
+                    .then(promiseValue => {
+                        console.log(promiseValue);
+                    })
