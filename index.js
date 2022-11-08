91cb544542c4aa952e5c58d6820e4969f7fcc046
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2022-11-08 15:59:11 +0200

    Read and Write

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require('fs').promises;
 Задача: прочитати вміст текстового файлу і вивести його на консоль
 */
 
-const promise = fs.readFile('./text.txt', 'utf-8')
-                    .then(promiseValue => {
-                        console.log(promiseValue);
-                    })
+const promise =fs.readFile('./text.txt', 'utf-8')
+.then (promiseValue => {
+    const fileText = promiseValue + 'We can read and write to files';
+    fs.writeFile('./newFile.txt', fileText)
+})
